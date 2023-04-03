840f3bf5920c2df4e582db6289307613bf3608a3
Andy Jung <jungkj@bc.edu>
2023-04-03 17:23:05 -0400

    v2 of zip unzip commands

--- unzip.c
+++ unzip.c
@@ -8,8 +8,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/* Use 16-bit code words */
-#define NUM_CODES 65536
+/* Use 12-bit code words */
+#define NUM_CODES 4096
 
 /* allocate space for and return a new string s+c */
 char *strappend_char(char *s, char c);
@@ -18,10 +18,12 @@ char *strappend_char(char *s, char c);
  * return NUM_CODES on EOF
  * return the code read otherwise
  */
-unsigned int read_code(int fd);
+unsigned int read_code(FILE *fd);
 
 /* uncompress in_file_name to out_file_name */
 void uncompress(char *in_file_name, char *out_file_name);
+
+unsigned int secondwrite; 
 
 int main(int argc, char **argv)
 {
@@ -34,9 +36,9 @@ int main(int argc, char **argv)
     char *in_file_name = argv[1];
     char *out_file_name = strdup(in_file_name);
     out_file_name[strlen(in_file_name)-4] = '\0';
-
     uncompress(in_file_name, out_file_name);
 
+    // have to free the memory for out_file_name as strdup() malloc()'ed it
     free(out_file_name);
 
     return 0;
@@ -64,68 +66,193 @@ char *strappend_char(char *s, char c)
  * return NUM_CODES on EOF
  * return the code read otherwise
  */
-unsigned int read_code(int fd)
+unsigned int read_code(FILE *fd)
 {
-    // code words are 16-bit unsigned shorts in the file
-    unsigned short actual_code;
-    int read_return = read(fd, &actual_code, sizeof(unsigned short));
-    if (read_return == 0)
-    {
-        return NUM_CODES;
+    if (secondwrite != NUM_CODES){
+    	unsigned int temp = secondwrite;
+	secondwrite = NUM_CODES;
+	if (temp == NUM_CODES -1){
+		temp = NUM_CODES;
+	}
+	return temp;
     }
-    if (read_return != sizeof(unsigned short))
-    {
-       perror("read");
-       exit(1);
+
+    unsigned char bitchange[3];
+    int numread = fread(bitchange, sizeof(unsigned char), 3, fd);
+    if (ferror(fd)){
+    	perror("fread");
+	exit(1);
     }
-    return (unsigned int)actual_code;
+
+    if (feof(fd)){
+    	return NUM_CODES;
+    }
+
+    if (numread != 3){
+    	printf("Error in input file");
+	exit(1);
+    }
+    //Take First CHAR 8 bits, get 4 bits from 2nd char 0
+    unsigned int onecode = ((bitchange[0] & 0xff) << 4) | ((bitchange[1] & 0xf0) >>4);
+    secondwrite = ((bitchange[1] & 0xf) <<8) | bitchange[2];
+    return onecode;
+
 }
 
 /* uncompress in_file_name to out_file_name */
 void uncompress(char *in_file_name, char *out_file_name)
 {
-    	char *dictionary[NUM_CODES];
-	int fd = open(in_file_name, O_RDONLY);
-	int fd2 = open(out_file_name, O_WRONLY|O_CREAT|O_TRUNC, 0644);
-	if (fd == -1){
-		printf("open failed");
-	}
+    // check for NULL args
+    if (in_file_name == NULL || out_file_name == NULL)
+    {
+        printf("Programming error!\n");
+        exit(1);
+    }
 
-	
-	
-	
-	//Initialize Dictionary
-	for (int i =0; i < 256; ++i){
-		char h = (char)i;
-		dictionary[i] = strappend_char("", h);
-	}
-	unsigned short currentcode;
-	unsigned int position = 256;
-	unsigned short nextcode;
-	char currentchar; 
-	char *currentstring;
-	currentcode = read_code(fd);
-	currentchar = *dictionary[currentcode];
-	write(fd2, &currentchar, sizeof(unsigned char));
-	while (currentcode > 0){
-		nextcode = read_code(fd);
-		if (dictionary[nextcode] == NULL){
-			currentstring = dictionary[currentcode];
-			currentstring = strappend_char(currentstring, currentchar);
-		}
-		else{
-			currentstring = dictionary[nextcode];
-			if (position < NUM_CODES){
-				++position;
-			}
-		}
-		write(fd2, currentstring, strlen(currentstring));
-		currentchar = currentstring[0];
-		char *oldstring = dictionary[currentcode];
-		dictionary[position] = strappend_char(oldstring, currentchar);
-		currentcode = nextcode;
-	}
-	close(fd);
-	close(fd2);
-	return;
+    // open both files
+    FILE *in_fd;
+    in_fd = fopen(in_file_name, "r");
+    if (in_fd == NULL)
+    {
+        perror(in_file_name);
+        exit(1);
+    }
+
+    FILE *out_fd;
+    out_fd = fopen(out_file_name, "w");
+    if (out_fd == NULL)
+    {
+        perror(out_file_name);
+        exit(1);
+    }
+
+    //  use an array as the dictionary
+    char *dictionary[NUM_CODES];
+    secondwrite = NUM_CODES;
+
+    // initialize the first 256 codes mapped to their ASCII equivalent as a string
+    unsigned int next_code;
+    for (next_code=0; next_code<256; next_code++)
+    {
+        dictionary[next_code] = strappend_char("\0", (char)next_code);
+    }
+
+    // initialize the rest of the dictionary to be NULLs
+    for (unsigned int i=256; i<NUM_CODES; ++i)
+    {
+        dictionary[i] = NULL;
+    }
+
+    // CurrentCode = first input code word
+    unsigned int cur_code = read_code(in_fd);
+    if (cur_code == NUM_CODES)
+    {
+        // if we hit EOF then we're done
+        return;
+    }
+
+    // CurrentChar = CurrentCode's string in Dictionary
+    char *cur_str = dictionary[cur_code];
+    if (cur_str == NULL)
+    {
+        printf("Algorithm error!\n");
+        exit(1);
+    }
+    int cur_str_length = strlen(cur_str);
+    if (cur_str_length != 1)
+    {
+        printf("Algorithm error!\n");
+        exit(1);
+    }
+
+    // Output CurrentChar
+    if (fwrite(cur_str, sizeof(char), cur_str_length, out_fd) != cur_str_length)
+    {
+        perror("write");
+        exit(1);
+    }
+
+    char cur_char = cur_str[0];
+
+    // have to track if cur_str needs to be free()'ed each iteration
+    int need_free = 0;
+
+    // While there are more input code words
+    // NextCode = next input code word
+    unsigned int new_code = read_code(in_fd);
+    while (new_code != NUM_CODES)
+    {
+        // check if cur_str needs to be free()'ed
+        if (need_free)
+        {
+            free(cur_str);
+            need_free = 0;
+        }
+
+        // If NextCode is NOT in Dictionary
+        if (dictionary[new_code] == NULL)
+        {
+            // CurrentString = CurrentCode's string in Dictionary
+            cur_str = dictionary[cur_code];
+
+            // CurrentString = CurrentString+CurrentChar
+            cur_str = strappend_char(cur_str, cur_char);
+
+            // track that cur_str was allocated and needs to be free()'ed later
+            need_free = 1;
+        }
+	else
+        {
+            // CurrentString = NextCode's String in Dictionary
+            cur_str = dictionary[new_code];
+
+            // note: don't free cur_str as it is pointing to memory in the dictionary
+        }
+
+        // Output CurrentString
+        cur_str_length = strlen(cur_str);
+        if (fwrite(cur_str, sizeof(char), cur_str_length, out_fd) != cur_str_length)
+        {
+            perror("write");
+            exit(1);
+        }
+
+        // CurrentChar = first character in CurrentString
+        cur_char = cur_str[0];
+
+        // only add another mapping to dictionary if there are codes left
+        if(next_code < NUM_CODES)
+        {
+            // OldString = CurrentCode's string in Dictionary
+            // Add OldString+CurrentChar to Dictionary
+            dictionary[next_code] = strappend_char(dictionary[cur_code], cur_char);
+            ++next_code;
+        }
+
+        // CurrentCode = NextCode
+        cur_code = new_code;
+        new_code = read_code(in_fd);
+    }
+
+    // close the files
+    if (fclose(in_fd) == EOF)
+    {
+        perror("close");
+    }
+    if (fclose(out_fd) == EOF)
+    {
+        perror("close");
+    }
+
+    // free all memory in the dictionary
+    for (unsigned int i=0; i<NUM_CODES; ++i)
+    {
+        if (dictionary[i] == NULL)
+        {
+            break;
+        }
+        free(dictionary[i]);
+    }
+
+    return;
 }
--- zip.c
+++ zip.c
@@ -8,9 +8,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/* Use 16-bit code words */
-#define NUM_CODES 65536
-
+/* Use 12-bit code words */
+#define NUM_CODES 4096
 /* allocate space for and return a new string s+t */
 char *strappend_str(char *s, char *t);
 
@@ -24,14 +23,17 @@ char *strappend_char(char *s, char c);
 unsigned int find_encoding(char *dictionary[], char *s);
 
 /* write the code for string s to file */
-void write_code(int fd, char *dictionary[], char *s);
+void write_code(FILE *fd, char *dictionary[], char *s);
 
 /* compress in_file_name to out_file_name */
 void compress(char *in_file_name, char *out_file_name);
 
+//global variable
+unsigned int secondwrite;
+
+
 int main(int argc, char **argv)
 {
-
     if (argc != 2)
     {
         printf("Usage: zip file\n");
@@ -40,12 +42,11 @@ int main(int argc, char **argv)
 
     char *in_file_name = argv[1];
     char *out_file_name = strappend_str(in_file_name, ".zip");
-
     compress(in_file_name, out_file_name);
 
-    /* have to free the memory for out_file_name since strappend_str malloc()'ed it */
+    // have to free the memory for out_file_name since strappend_str malloc()'ed it
     free(out_file_name);
-
+	
     return 0;
 }
 
@@ -113,78 +114,197 @@ unsigned int find_encoding(char *dictionary[], char *s)
 }
 
 /* write the code for string s to file */
-void write_code(int fd, char *dictionary[], char *s)
+void write_code(FILE *fd, char *dictionary[], char *s)
 {
-    if (dictionary == NULL || s == NULL)
+    if (dictionary == NULL)
     {
         return;
     }
 
-    unsigned int code = find_encoding(dictionary, s);
-    // should never call write_code() unless s is in the dictionary 
-    if (code == NUM_CODES)
-    {
-        printf("Algorithm error!");
-        exit(1);
+    unsigned int code;
+    
+    //Check is s is NULL then we use 4095
+    if (s == NULL){
+    	code = NUM_CODES-1;
+    }
+    else{
+    	code = find_encoding(dictionary, s);
+	if (code == NUM_CODES)
+	{
+		printf("Algorithm error!");
+		exit(1);
+	}
+    }
+    
+    // cast the code to an unsigned short to only use 12 bits per code word in the output file
+  
+    if (secondwrite == NUM_CODES){
+    	secondwrite = (unsigned int)code;
+	return;
+    }
+    
+    unsigned char bitchange[3]; 
+    //take right 8 bits
+    bitchange[0] = secondwrite >> 4;
+    //takes left 8 bits, code's right 4 bits, OR Them 
+    bitchange[1] = (secondwrite << 4) | (code >> 8);
+    //last char
+    bitchange[2] = code;
+
+    if ((fwrite(bitchange, sizeof(unsigned char), 3, fd) != 3)  || ferror(fd)){
+    	perror("fwrite");
+	exit(1);
     }
 
-    // cast the code to an unsigned short to only use 16 bits per code word in the output file
-    unsigned short actual_code = (unsigned short)code;
-    if (write(fd, &actual_code, sizeof(unsigned short)) != sizeof(unsigned short))
-    {
-        perror("write");
-        exit(1);
-    }
+    secondwrite = NUM_CODES;
 }
 
 /* compress in_file_name to out_file_name */
 void compress(char *in_file_name, char *out_file_name)
 {
-    	char *dictionary[NUM_CODES];
-	int fd = open(in_file_name, O_RDONLY);
-	if (fd == -1){
-		printf("open failed");
-	}	
+    // check for bad args
+    if (in_file_name == NULL || out_file_name == NULL)
+    {
+        printf("Programming error!\n");
+        exit(1);
+    }
 
-	if (in_file_name[0] == '\0'){
-		fd = 0;
-	}
+    // open both files
+ 
+    FILE *in_fd; 
+    in_fd = fopen(in_file_name, "r");
+    if (in_fd == NULL)
+    {
+        perror(in_file_name);
+        exit(1);
+    }
 
-	int fd2 = open(out_file_name, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    FILE *out_fd;
+    out_fd = fopen(out_file_name, "w");
+    if (out_fd == NULL)
+    {
+        perror(out_file_name);
+        exit(1);
+    }
 
-	//Pseudocode Convert
-	
-	//Initialize Dictionary
-	for (int i = 0; i < 256; ++i){
-		char h = (char)i;
-		dictionary[i] = strappend_char("", h);
-	}
-	
-	
-	char c;
-	char *currentstring = strappend_char("", c);
-	unsigned int position = 256;
-	while (read(fd, &c, sizeof(unsigned char)) > 0){
-		char *copy = strdup(currentstring);
-		free(currentstring);
-		char *together = strappend_char(copy, c);
-		if (find_encoding(dictionary, together) != NUM_CODES){
-			currentstring = strdup(together);
-		}
-		else{
-			write_code(fd2, dictionary, copy);
-			if (position < NUM_CODES){
-				dictionary[position] = strdup(together);
-				++position;
-			}
-			currentstring = strappend_char("", c);
-		}
-		free(copy);
-		free(together);
-	}
-	
-	close(fd);
-	close(fd2);
-		
-	return;
+    // use an array as the dictionary
+    char *dictionary[NUM_CODES];
+    secondwrite = NUM_CODES;
+    
+    // initialize the first 256 codes mapped to their ASCII equivalent as a string
+    unsigned int next_code;
+    for (next_code=0; next_code<256; ++next_code)
+    {
+        dictionary[next_code] = strappend_char("\0", (char)next_code);
+    }
+
+    // initialize the rest of the dictionary to be NULLs
+    for (unsigned int i=256; i<NUM_CODES; ++i)
+    {
+        dictionary[i] = NULL;
+    }
+
+    char *cur_str;
+    char cur_char;
+
+    // CurrentString = first input character
+    fread(&cur_char, sizeof(char), 1,in_fd);
+    if (ferror(in_fd))
+    {
+        perror("fread");
+        exit(1);
+    }
+    if (feof(in_fd))
+    {
+        // if the file is empty, we're done
+        return;
+    }
+    cur_str = strappend_char("\0", cur_char);
+
+    // CurrentChar = next input character
+    fread(&cur_char, sizeof(char),1 ,in_fd);
+    if (ferror(in_fd))
+    {
+	    perror("fread");
+	    exit(1);
+    }
+
+    // While there are more input characters
+    while (!feof(in_fd))
+    {
+        // hold CurrentString+CurrentChar in a temp variable
+        char *tmp = strappend_char(cur_str, cur_char);
+
+        // If CurrentString+CurrentChar is in Dictionary
+        unsigned int code = find_encoding(dictionary, tmp);
+        if (code != NUM_CODES)
+        {
+            // CurrentString = CurrentString+CurrentChar
+            char *hold = strappend_char(cur_str, cur_char);
+
+            // have to free the memory for cur_str before we reassign it
+            free(cur_str);
+            cur_str = hold;
+        }
+        else
+        {
+            // only add tmp to dictionary if there are codes left 
+            if (next_code < NUM_CODES - 1)
+            {
+                // Add CurrentString+CurrentChar to Dictionary
+                dictionary[next_code] = strdup(tmp);
+                ++next_code;
+            }
+
+            // CodeWord = CurrentString's code in Dictionary
+            // Output CodeWord
+            write_code(out_fd, dictionary, cur_str);
+	    free(cur_str);
+
+            // CurrentString = CurrentChar
+            cur_str = strappend_char("\0", cur_char);
+        }
+
+        // don't forget to free tmp
+        free(tmp);
+
+        // read the next char
+        fread(&cur_char, sizeof(char), 1, in_fd);
+        if (ferror(in_fd))
+        {
+            perror("read");
+            exit(1);
+        }
+    }
+
+    // CodeWord = CurrentString's code in Dictionary
+    // Output CodeWord
+    write_code(out_fd, dictionary, cur_str);
+
+    // free the last memory allocated for cur_str
+    free(cur_str);
+
+    write_code(out_fd, dictionary, NULL);
+    // close the files
+    if (fclose(in_fd) != 0)
+    {
+        perror("close");
+    }
+    if (fclose(out_fd) != 0)
+    {
+        perror("close");
+    }
+
+    // free all memory in the dictionary
+    for (unsigned int i=0; i<NUM_CODES; ++i)
+    {
+        if (dictionary[i] == NULL)
+        {
+            break;
+
+        }
+        free(dictionary[i]);
+    }
+
+    return;
 }
